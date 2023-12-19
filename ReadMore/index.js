8ea87318f9fc7181273f59ffd5e1226c29760b78
Aakash Bhadana <aakashbhadana@outlook.com>
2023-12-19 19:36:40 +0530

    Update index.js

--- ReadMore/index.js
+++ ReadMore/index.js
@@ -8,7 +8,7 @@ function ReadMore({ height = "h-24", className = "", children }) {
 	return (
 		<div className="relative">
 			<div className={`${Expand ? "h-auto" : height} overflow-hidden ${className}`}>{children}</div>
-			<div className="absolute w-full h-12 bottom-8" style={{ background: "linear-gradient(to bottom, rgba(255,255,255,0) 20%,rgba(255,255,255,1))" }} />
+			<div className="absolute w-full h-12 bottom-8" style={{ background: Expand ? "" : "linear-gradient(to bottom, rgba(255,255,255,0) 20%,rgba(255,255,255,1))" }} />
 			<Button onClick={() => setExpand(!Expand)} className="mt-2 btn-xs" variant="outlined">
 				More
 				{Expand ? <FiChevronUp /> : <FiChevronDown />}
