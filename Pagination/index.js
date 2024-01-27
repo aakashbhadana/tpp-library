9a965d4d60449bc7e5c80ae528fcbff5d56a9339
Aakash Bhadana <aakashbhadana@outlook.com>
2024-01-27 16:38:11 +0530

    -

--- Pagination/index.js
+++ Pagination/index.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import React from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-function Pagination({total=1, active=0, onChange=()=>{}, className=''}) {
-
-    return (
-        <nav className={`pagination ${className}`} aria-label="Pagination">
-            <div className='page rounded-l-md'>
-              <span className="sr-only">Previous</span>
-              <FiChevronLeft className="h-5 w-5" aria-hidden="true" />
-            </div>
-            {
-                [...Array(total)].map((e, i) => {
-                    return (
-                        <div key={i} onClick={()=>onChange(i+1)} className={`page ${active===i+1 ? 'active' : ''}`}>{i+1}</div>
-                    )
-                })
-            }
-            <div className='page rounded-r-md'>
-              <span className="sr-only">Next</span>
-              <FiChevronRight className="h-5 w-5" aria-hidden="true" />
-            </div>
-          </nav>
-    );
+function Pagination({ total = 1, active = 0, onChange = () => {}, className = "" }) {
+	return (
+		<nav className={`pagination ${className}`} aria-label="Pagination">
+			<div className="page rounded-l-md">
+				<span className="sr-only">Previous</span>
+				<FiChevronLeft className="w-5 h-5" aria-hidden="true" />
+			</div>
+			<div className="max-w-[32rem] overflow-x-auto flex gap-1 scrollbar-hidden">
+				{[...Array(total)].map((e, i) => {
+					return (
+						<div key={i} onClick={() => onChange(i + 1)} className={`page ${active === i + 1 ? "active" : ""}`}>
+							{i + 1}
+						</div>
+					);
+				})}
+			</div>
+			<div className="page rounded-r-md">
+				<span className="sr-only">Next</span>
+				<FiChevronRight className="w-5 h-5" aria-hidden="true" />
+			</div>
+		</nav>
+	);
 }
 
 export default Pagination;
